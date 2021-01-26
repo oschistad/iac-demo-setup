b884b7ed8184936e364ff12ef9a086e3d744ed33
OleAndréSchistad <oas@aztek.no>
2021-01-26 07:54:11 +0100

    Configure AzureRM provider

--- main.tf
+++ main.tf
@@ -8,11 +8,18 @@ https://docs.microsoft.com/en-us/azure/virtual-machines/linux/image-builder
 https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/shared_image
 https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_client_secret
 */
+terraform {
+  required_providers {
+    azurerm = {
+      version = "=2.40.0"
+    }
+  }
+}
 # Configure the Microsoft Azure Provider
+
 provider "azurerm" {
   # We recommend pinning to the specific version of the Azure Provider you're using
   # since new versions are released frequently
-  version = "=2.40.0"
 
   features {}
 
@@ -24,12 +31,13 @@ provider "azurerm" {
   # client_id       = "..."
   # client_secret   = "..."
   # tenant_id       = "..."
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
-resource "azurerm_user_assigned_identity" "" {
-  location = ""
-  name = ""
-  resource_group_name = ""
-}
+
+
 resource "azurerm_resource_group" "builder"{
 
   location = "Norway East"
